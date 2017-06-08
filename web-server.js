6120966ccf58c24318d32e96b97fdd060a72f67d
Hakim Ahmim <hahmim@gmail.com>
2017-06-08 18:50:43 -0400

    update about us

--- web-server.js
+++ web-server.js
@@ -23,7 +23,7 @@ app.use(middleware.logger)
 var port = process.env.Port;
 
 app.get('/about',middleware.requireAuthentication,function (req,res) {
-    res.send("About Us")
+    res.send("About Us!")
 })
 app.use(express.static(__dirname+'/public'));
 // console.log(__dirname);
