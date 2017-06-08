8e97952f591ce005a95ae5157b1d0c76fbe17803
Hakim Ahmim <hahmim@gmail.com>
2017-06-08 19:04:32 -0400

    modularized app

--- web-server.js
+++ web-server.js
@@ -4,19 +4,8 @@
 var express = require('express');
 var app = express();
 require('dotenv').load()
+var middleware = require('./middleware.js')
 
-var middleware ={
-    requireAuthentication:function (req,res,next) {
-        console.log('private route hit');
-        next();
-    },
-    logger:function (req,res,next) {
-
-        console.log('Request: '+new Date().toString()+' '+req.method+' '+ req.originalUrl);
-
-        next();
-    }
-};
 // app.use(middleware.requireAuthentication)
 app.use(middleware.logger)
 
